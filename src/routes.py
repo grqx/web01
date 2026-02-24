09990bdf64f76d1e17c06ba1acd9e4a24f386225
grchz <grqz.dev@proton.me>
2026-02-25 09:33:07 +1300

    add proper bike page

--- src/routes.py
+++ src/routes.py
@@ -10,5 +10,12 @@ def _root():
             r'SELECT bike_id, maker_id, model FROM Bikes').execute(
                 con.cursor()).fetchall())
 
+@app.route('/bike/<int:bike_id>')
+def _bike(bike_id: int):
+    with lazy_con() as con:
+        return render_template('bike.html', bike=Select(
+            r'SELECT * FROM Bikes').where('bike_id = ?').execute(
+                con.cursor(), (bike_id, )).fetchone())
+
 
 __all__ = []
