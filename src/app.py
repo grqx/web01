e7c8425de71fe727343cba29c785ef08cd8cfa99
grchz <grqz.dev@proton.me>
2026-03-02 23:42:03 +1300

    add some auth

--- src/app.py
+++ src/app.py
@@ -1,3 +1,10 @@
 from flask import Flask
 
-app = Flask('app', static_folder='static', template_folder='templates')
+app: Flask | None = None
+
+def run_app():
+    global app
+    app = Flask('app', static_folder='static', template_folder='templates')
+    from .routes import init_routes
+    init_routes()
+    app.run(debug=True)
--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,18 @@
-from flask import render_template
+import bcrypt
+
+from flask import redirect, render_template, request, Response, session
+from uuid import uuid4
 from .db_op import lazy_con
 from .sqlbld import Select
 from .app import app
+
+assert app, 'app must not be none at the time of import'
+
+ADMIN_PASSWD = b'$2b$12$umixqSvkhVhA.USi6QAokO.duMRKOGZyjwV.uzhRyfBPfbORpchLy'
+
+
+def init_routes():
+    app.secret_key = uuid4().bytes
 
 @app.route('/')
 def _root():
@@ -16,6 +27,29 @@ def _bike(bike_id: int):
         return render_template('bike.html', bike=Select(
             r'SELECT * FROM Bikes').where('bike_id = ?').execute(
                 con.cursor(), (bike_id, )).fetchone())
+
+@app.route('/login', methods=['GET', 'POST'])
+def _login():
+    if request.method == 'POST':
+        k = request.form.get('k')
+        if not k:
+            return Response(
+                'Expected "k" in form', status=400,
+            )
+        if not bcrypt.checkpw(k.encode(), ADMIN_PASSWD):
+            return Response(
+                'Password incorrect', status=403,
+            )
+        session['l'] = True
+        return redirect('/admin')
+
+    if session.get('l') is True:
+        return redirect('/admin')
+    return render_template('login.html')
+
+@app.route('/admin')
+def _admin():
+    return 'Hello, admin!'
 
 
 __all__ = []
